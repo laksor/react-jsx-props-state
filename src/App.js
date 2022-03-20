0c1405d0fe50fc38e1da7013464af5e17f903cf7
laskor <al.moksud@gmail.com>
2022-03-19 23:21:40 -0400

    creating componenet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
-const number = 888;
-const singer = {name: 'lala', job: 'singer'};
-const singerStyle = {
-  color: 'red',
-  backgroundColor: 'white',
-  padding: '10px'
-}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>JSX</h1>
-        <p>number: {number}</p>
-        <div className='container'>
-          <h3>Hello react, how are you</h3>
-        </div> 
-        <div className="music">
-          <p style={singerStyle}>Name : {singer.name} , job : {singer.job}</p>
-        </div>
-
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
+}
+
+function Person() {
+  return (
+    <div className='person'>
+      <h1>shakib al hasan</h1>
+      <p>professon: cricketer</p>
+    </div>
+  )
 }
 
 export default App;
