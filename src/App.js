810df39498a72f58f5633b8dc2a42485cddaa297
laskor <al.moksud@gmail.com>
2022-03-20 16:30:27 -0400

    display array

--- src/App.js
+++ src/App.js
@@ -2,30 +2,47 @@
 import './App.css';
 
 function App() {
-  const nayoks = ['Rubel', 'Taskin', 'Bappa', 'salman'];
+  /*const nayoks = ['Rubel', 'Taskin', 'Bappa', 'salman', 'mojumdur'];*/
+  const products = [
+    {name : 'Lenovo', price: 3000},
+    {name : 'Apple', price: 15000},
+    {name : 'samsung', price: 12000},
+    {name : 'htc', price: 6000},
+  ];
   return (
     <div className="App">
       {
-        nayoks.map(nayok => <Person name={nayok}></Person>)
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
       }
-      {/*<Person name={nayoks[0]} job="cricketer"></Person>
+
+      {/*{nayoks.map(nayok => <Person name={nayok}></Person>)}
+      <Person name={nayoks[0]} job="cricketer"></Person>
       <Person name={nayoks[1]}  job="cricketer"></Person>
       <Person name={nayoks[2]}  job="Hero"></Person>
-    <Person></Person>*/}
+    <Person></Person>
       <h1>New component</h1>
       <Friend></Friend>
-      <Friend></Friend>
+    <Friend></Friend>*/}
     </div>
   );
 }
 
-function Person(props) {
+function Product(props){
+  return(
+    <div className='product'>
+      <h1>Brand: {props.name}</h1>
+      <p>price: {props.price}</p>
+    </div>
+  );
+}
+
+/*function Person(props) {
   return (
     <div className='person'>
       <h1>{props.name}</h1>
       <p>{props.job}</p>
     </div>
-  )
+  );
 }
 
 function Friend() {
@@ -34,7 +51,7 @@ function Friend() {
       <h3>Name: Ajay dev</h3>
       <p>Job: Maramari</p>
     </div>
-  )
-}
+  );
+}*/
 
 export default App;
