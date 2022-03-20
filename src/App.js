6251d7343746dc403195a4431163d278759bbd63
laskor <al.moksud@gmail.com>
2022-03-19 23:00:45 -0400

    jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+const number = 888;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className='container'>
+          <h3>Hello react, how are you</h3>
+        </div> 
+        <div className="music">
+          <p>Name : {555 + number}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
