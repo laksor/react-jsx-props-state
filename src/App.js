a277577dea447e5f81c9174aefb9a5c987535c85
laskor <al.moksud@gmail.com>
2022-03-19 23:08:23 -0400

    dynamic

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 const number = 888;
+const singer = {name: 'lala', job: 'singer'};
+const singerStyle = {
+  color: 'red',
+  backgroundColor: 'white',
+  padding: '10px'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>JSX</h1>
+        <p>number: {number}</p>
         <div className='container'>
           <h3>Hello react, how are you</h3>
         </div> 
         <div className="music">
-          <p>Name : {555 + number}</p>
+          <p style={singerStyle}>Name : {singer.name} , job : {singer.job}</p>
         </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
