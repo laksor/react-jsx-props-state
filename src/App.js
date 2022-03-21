b59f11fc0a91b5da6b9db90496e9dede6e028770
laskor <al.moksud@gmail.com>
2022-03-21 18:32:53 -0400

    api

--- src/App.js
+++ src/App.js
@@ -28,17 +28,31 @@ function App() {
     </div>
   );
 }
-function ExternalUsers(props) {
+function ExternalUsers() {
   const [users, setUsers] = useState([]);
   useEffect( ()=>{
-    
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
   } , []);
+
   return(
     <div>
-      <h1></h1>
-      <p></p>
+      <h1>External users</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
     </div>
   );
+}
+
+function User(props){
+  return(
+    <div style={{border: '2px solid red', margin: '20px'}}>
+      <h3>Name: {props.name}</h3>
+      <h3>Email: {props.email}</h3>
+    </div>
+  )
 }
 /*function Counter(){
   const [count, setCount] = useState(55);
