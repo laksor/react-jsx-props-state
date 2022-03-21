b3b7e457dc2b3d0319ed148422b682d981c7a32b
laskor <al.moksud@gmail.com>
2022-03-20 22:44:23 -0400

    usestate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  /*const nayoks = ['Rubel', 'Taskin', 'Bappa', 'salman', 'mojumdur'];*/
+  /*const nayoks = ['Rubel', 'Taskin', 'Bappa', 'salman', 'mojumdur'];
   const products = [
     {name : 'Lenovo', price: 3000},
     {name : 'Apple', price: 15000},
     {name : 'samsung', price: 12000},
     {name : 'htc', price: 6000},
-  ];
+  ];*/
   return (
     <div className="App">
-      {
+      <Counter></Counter>
+
+
+
+      {/*{
         products.map(product => <Product name={product.name} price={product.price}></Product>)
       }
-
-      {/*{nayoks.map(nayok => <Person name={nayok}></Person>)}
+      {nayoks.map(nayok => <Person name={nayok}></Person>)}
       <Person name={nayoks[0]} job="cricketer"></Person>
       <Person name={nayoks[1]}  job="cricketer"></Person>
       <Person name={nayoks[2]}  job="Hero"></Person>
-    <Person></Person>
+      <Person></Person>
       <h1>New component</h1>
       <Friend></Friend>
-    <Friend></Friend>*/}
+      <Friend></Friend>*/}
     </div>
   );
 }
 
-function Product(props){
+function Counter(){
+  const [count, setCount] = useState(55);
+  const increaseCount = () =>{
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button type='submit' onClick={increaseCount}>Increase</button>
+    </div>
+  );
+}
+
+/*function Product(props){
   return(
     <div className='product'>
       <h1>Brand: {props.name}</h1>
@@ -36,7 +54,7 @@ function Product(props){
   );
 }
 
-/*function Person(props) {
+function Person(props) {
   return (
     <div className='person'>
       <h1>{props.name}</h1>
